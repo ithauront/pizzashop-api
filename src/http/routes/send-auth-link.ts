0034f339b7e06ab9d9ff21bd9ccd15c456ce8029
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-01-20 19:55:11 +0100

    [feat]: send email on send auth link

--- src/http/routes/send-auth-link.ts
+++ src/http/routes/send-auth-link.ts
@@ -3,6 +3,8 @@ import { db } from '../../db/connection'
 import { createId } from '@paralleldrive/cuid2'
 import { authLinks } from '../../db/schema'
 import { env } from '../../env'
+import { mail } from '../../lib/mail'
+import nodemailer from 'nodemailer'
 
 export const sendAuthLink = new Elysia().post(
   '/authenticate',
@@ -31,6 +33,19 @@ export const sendAuthLink = new Elysia().post(
     authLink.searchParams.set('code', authLinkCode)
     authLink.searchParams.set('redirect', env.AUTH_REDIRECT_URL)
 
+    const info = await mail.sendMail({
+      from: {
+        name: 'Pizzashop',
+        address: 'hi@pizzashop.com',
+      },
+      to: {
+        name: userFromEmail.name,
+        address: email,
+      },
+      subject: 'Authenticate to Pizza shop',
+      text: `Use the following link to authenticate on Pizzashop: ${authLink.toString()}`,
+    })
+    console.log(nodemailer.getTestMessageUrl(info))
     console.log(authLink.toString())
   },
   {
