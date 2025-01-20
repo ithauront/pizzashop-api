633a7a53528cbedd7cd9b06238725d634986773b
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-01-20 10:44:36 +0100

    [feat]: get managed restaurant route

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,12 +3,14 @@ import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { getProfile } from './routes/get-profile'
+import { getManagedRestaurant } from './routes/get-managed-restaurant'
 
 const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
   .use(getProfile)
+  .use(getManagedRestaurant)
 
 app.listen(3333, () => {
   console.log('🚀 HTTP server running')
